23bac4de26eb32ba817a0c0d0b68433ac7073a14
tasuku <tasuku1215mhg@gmail.com>
2018-06-14 09:20:32 +0900

    a

---
+++ PH11/public_html/2018-06-13nyuryoku/index.php
@@ -0,0 +1,81 @@
+<?php
+require_once './func.php';
+$error['name']='';
+$error['tel']='';
+$error['mail']='';
+$error[0]=0;
+$name='';
+$tel='';
+$mail='';
+if(isset($_GET['enter'])){
+  if($_GET['name']=='')
+  {
+    $error['name']='未入力です';
+  }
+  else
+  {
+    $name=h($_GET['name']);
+    $error[0]+=1;
+  }
+  if($_GET['tel']=='')
+  {
+    $error['tel']='未入力です';
+  }
+  else
+  {
+    $tel=h($_GET['tel']);
+    $error[0]+=1;
+  }
+  if($_GET['mail']=='')
+  {
+    $error['mail']='未入力です';
+  }
+  else
+  {
+    $mail=h($_GET['mail']);
+    $error[0]+=1;
+  }
+}
+?>
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+ <head>
+  <meta charset="utf-8">
+  <title>php</title>
+  <meta name="description" content="ページの内容を記述">
+  <meta name="keywords" content="キーワード,カンマで区切り,指定">
+  <link rel="stylesheet" href="./css/reset.css">
+  <link rel="stylesheet" href="./css/style.css">
+  <script src="./js/jquery.js" charset="utf-8"></script>
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+  <![endif]-->
+  <script src="sample.js"></script>
+  </head>
+  <body>
+    <form action="index.php" method="get">
+      <table>
+        <tr>
+          <th>名前</th>
+          <td><input type="text" name="name" value="<?php echo $name;?>"></td>
+          <td><?php echo $error['name']; ?></td>
+        </tr>
+        <tr>
+          <th>電話番号</th>
+          <td><input type="text" name="tel" value="<?php echo $tel;?>"></td>
+          <td><?php echo $error['tel']; ?></td>
+        </tr>
+        <tr>
+          <th>メール</th>
+          <td><input type="text" name="mail" value="<?php echo $mail;?>"></td>
+          <td><?php echo $error['mail']; ?></td>
+        </tr>
+          <tr>
+          <td></td>
+          <td><input type="submit" name='enter' value="送信"></td>
+        </tr>
+      </table>
+    </form>
+  </body>
+</html>
